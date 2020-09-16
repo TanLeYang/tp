5a99ca48e4a5a964ea240264d4f9f5a0fc4a667e
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-09-16 13:23:20 +0800

    Add tests for remark command

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -7,7 +9,7 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Remark;
 
-import java.util.List;
+
 
 public class RemarkCommand extends Command {
 
